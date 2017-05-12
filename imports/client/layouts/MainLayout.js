40c1ce0a45c8436e1efc94894ac4be8f1f0ddf7d
Benjamin Krier <benkrier@gmail.com>
2017-05-12 12:25:32 -0500

    Change app title

--- imports/client/layouts/MainLayout.js
+++ imports/client/layouts/MainLayout.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router';
 const MainLayout = ({children}) =>
   <div className="main-layout">
     <header>
-      <h1><Link to='/'>Level Up Voting</Link></h1>
+      <h1><Link to='/'>This and That Voting App</Link></h1>
       <LoginButtons />
       <nav>
         <Link to='/about'>About</Link>
